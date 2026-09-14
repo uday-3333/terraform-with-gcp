9664f5fe1d7959bbb47e03ec227e0beb960ef5a7
uday nalluri <udayabhaskar.n@nrg.com>
2026-09-14 11:39:52 -0500

    fixed IAM permissions

--- poc-gcp/service-accounts.tf
+++ poc-gcp/service-accounts.tf
@@ -7,10 +7,22 @@ resource "google_service_account" "cloud_run_sa" {
   description = "Service account for Cloud Run services in ${local.environment} environment"
 }
 
+# Allow unauthenticated public access to Cloud Run services
+resource "google_cloud_run_v2_service_iam_member" "public_invoker" {
+  for_each = local.cloud_run_services
+
+  project  = local.project_id
+  location = local.region
+  name     = each.value.service_name
+  role     = "roles/run.invoker"
+  member   = "allUsers"
+
+  depends_on = [module.cloud_run]
+}
+
 # Assign necessary roles to Cloud Run service account
 resource "google_project_iam_member" "cloud_run_roles" {
   for_each = toset([
-    "roles/run.invoker",
     "roles/cloudsql.client",
     "roles/secretmanager.secretAccessor",
   ])
